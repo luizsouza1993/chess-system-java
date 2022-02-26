279e8daf2e4eb273eb4ce68597074e1e710e5b66
Luiz <santosfc10@hotmail.com>
2022-02-25 21:41:19 -0300

    Layer and printBoard

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,14 @@
 package application;
 
-import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
 			
-		Position pos = new Position(3,5);
-		
-		System.out.print(pos.toString());
+		ChessMatch chessmatch = new ChessMatch();
+			UI.printBoard(chessmatch.getPieces());
+
 
 	}
 
--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,5 +1,46 @@
 package boardgame;
 
 public class Board {
+	
+	private int rows;
+	private int cols;
+	private Piece[][] pieces;
+	
+	
+	public Board(int rows, int cols) {
+		this.rows = rows;
+		this.cols = cols;
+		pieces = new Piece[rows][cols];
+	}
+
+
+	public int getRows() {
+		return rows;
+	}
+
+
+	public void setRows(int rows) {
+		this.rows = rows;
+	}
+
+
+	public int getCols() {
+		return cols;
+	}
+
+
+	public void setCols(int cols) {
+		this.cols = cols;
+	}
+	
+	public Piece piece (int row, int column) {
+		return this.pieces[row][column];
+	}
+	
+	public Piece piece (Position position) {
+		return this.pieces[position.getRow()][position.getCol()];
+	}
+	
+	
 
 }
